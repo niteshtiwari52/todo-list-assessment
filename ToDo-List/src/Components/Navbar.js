aa146d0c3b03fa169bb04dc31d2612525dc5a128
niteshtiwari52 <niteshtiwari5222@gmail.com>
2024-02-18 04:40:17 +0530

    homapage redirection Url correction

--- ToDo-List/src/Components/Navbar.js
+++ ToDo-List/src/Components/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = () => {
     <>
       <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="http:/localhost:3000" className="flex items-center">
+          <a href="/" className="flex items-center">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               My-Task
             </span>
